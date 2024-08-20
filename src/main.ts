fce2d55ec5b9f1bd7fb14123b9c14888fbc614d1
Grenier Celestin <celestin.grenier@hotmail.com>
2024-08-20 13:28:47 +0200

    maybe pwa (untested)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, { providers: [] }).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+] }).catch((err) => console.error(err));
